5698f35af606daf1f6dc9975aed37b1f8051d8f5
Andrew DeMarco <97972359+andydemarks@users.noreply.github.com>
2022-05-18 16:46:54 -0400

    finished the my2cents component
    added rest of content/updated animations to both portfolio and my 2 cents components.

--- DeMarksWebsite/src/app/portfolio/portfolio.component.ts
+++ DeMarksWebsite/src/app/portfolio/portfolio.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { transition, trigger, useAnimation } from '@angular/animations';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { fadeIn } from 'ng-animate';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
+  styleUrls: ['./portfolio.component.css'],
+  animations: [
+    trigger('fadeInState', [
+      transition('void => *', useAnimation(fadeIn, {
+        // Set the duration to 5seconds and delay to 2seconds
+        params: { timing: 1 }
+      }))
+    ])
+  ]
 })
-export class PortfolioComponent implements OnInit {
+export class PortfolioComponent implements AfterViewInit {
+  transition:string = 'none';
 
   constructor() { }
-
-  ngOnInit(): void {
+  
+  ngAfterViewInit(): void {
+    this.transition = 'none';
   }
 
 }
